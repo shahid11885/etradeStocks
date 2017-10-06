e8ac5be4d9cb0696d3a9a6eb990f3acdf8758c44
Shahid Ahmed <shahid.ahmed@me.com>
2017-10-06 16:08:51 -0700

    Basic functional program

--- app/services/EtradeRequestTokenService.java
+++ app/services/EtradeRequestTokenService.java
@@ -11,25 +11,24 @@ import com.etrade.etws.sdk.common.ETWSException;
 
 import java.io.IOException;
 
-
 public class EtradeRequestTokenService {
-    public IOAuthClient  client              = null;
+    public IOAuthClient  client             = null;
     public ClientRequest request            = null;
     public Token token                      = null;
-    public String oauth_consumer_key        = null; // Your consumer key
-    public String oauth_consumer_secret     = null; // Your consumer secret
-    public String oauth_request_token       = null; // Request token
-    public String oauth_request_token_secret= null; // Request token secret
+    public String oauth_consumer_key        = null;                     // Your consumer key
+    public String oauth_consumer_secret     = null;                     // Your consumer secret
+    public String oauth_request_token       = null;                     // Request token
+    public String oauth_request_token_secret= null;                     // Request token secret
 
     public EtradeRequestTokenService() throws IOException, ETWSException {
-        client      = OAuthClientImpl.getInstance(); // Instantiate IOAUthClient
-        request     = new ClientRequest(); // Instantiate ClientRequest
-        request.setEnv(Environment.SANDBOX); // Use sandbox environment
-
-        request.setConsumerKey(oauth_consumer_key); //Set consumer key
-        request.setConsumerSecret(oauth_consumer_secret); // Set consumer secret
-        token                       = client.getRequestToken(request); // Get request-token object
-        oauth_request_token         = token.getToken(); // Get token string
-        oauth_request_token_secret  = token.getSecret(); // Get token secret
+        client      			= OAuthClientImpl.getInstance();// Instantiate IOAUthClient
+        request     			= new ClientRequest();          // Instantiate ClientRequest
+        request.setEnv(Environment.SANDBOX);                            // Use sandbox environment
+    
+        request.setConsumerKey(oauth_consumer_key);                     // Set consumer key
+        request.setConsumerSecret(oauth_consumer_secret);               // Set consumer secret
+        token                       	= client.getRequestToken(request);  // Get request-token object
+        oauth_request_token         	= token.getToken();             // Get token string
+        oauth_request_token_secret  	= token.getSecret();            // Get token secret
     }
 }
